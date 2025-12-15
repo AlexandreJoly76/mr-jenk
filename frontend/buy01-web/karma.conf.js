d4762a36f52514f296233cb0c9b70da7f5762f71
ajoly <alexandre.joly.76300@gmail.com>
2025-12-15 17:16:42 +0100

    test

--- frontend/buy01-web/karma.conf.js
+++ frontend/buy01-web/karma.conf.js
@@ -31,7 +31,19 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
     browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
